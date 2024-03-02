1f095753aae7ef1887c1093c04839e25a1c0d8ce
chaabat <chaabat.02@gmail.com>
2024-03-02 15:22:32 +0100

    model admin,utilis,organis

--- database/migrations/2024_03_02_141524_create_utilisateurs_table.php
+++ database/migrations/2024_03_02_141524_create_utilisateurs_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('utilisateurs', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
             $table->timestamps();
         });
     }
