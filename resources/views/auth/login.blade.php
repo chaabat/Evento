4a6209413ccb73257ae2721cf36fc5d0f68e773e
chaabat <chaabat.02@gmail.com>
2024-03-03 16:18:34 +0100

    login page UI

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
@@ -44,4 +44,63 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</x-guest-layout> --}}
+
+@extends('layouts.master')
+@section('login')
+    <div class="w-full min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0"
+        style="background:linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('{{ asset('photos/event.jpg') }}') no-repeat center;background-size:cover">
+        <a href=""><img src="{{ asset('photos/logoo.png') }}" alt="Logo"></a>
+
+        <div class="w-full sm:max-w-md p-5 mx-auto">
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+
+                <div class="mb-4">
+                    <label class="block mb-1 text-white font-mono font-bold" for="email">Email-Address</label>
+                    <input id="email" type="text" name="email"
+                        class="py-2 px-3 border border-white focus:border-red-300 focus:outline-none focus:ring focus:ring-red-200 focus:ring-opacity-50 rounded-md shadow-sm disabled:bg-gray-100 mt-1 block w-full"
+                        value="{{ old('email') }}" required autofocus autocomplete="username" />
+                    @error('email')
+                        <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
+                    @enderror
+                </div>
+
+                <div class="mb-4">
+                    <label class="block mb-1 text-white font-mono font-bold" for="password">Mot de passe</label>
+                    <input id="password" type="password" name="password"
+                        class="py-2 px-3 border border-bg-white focus:border-red-300 focus:outline-none focus:ring focus:ring-red-200 focus:ring-opacity-50 rounded-md shadow-sm disabled:bg-gray-100 mt-1 block w-full"
+                        required autocomplete="current-password" />
+                    @error('password')
+                        <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
+                    @enderror
+                </div>
+
+                <div class="mt-6 flex items-center justify-between">
+                    <div class="flex items-center">
+                        <label for="remember_me" class="inline-flex items-center">
+                            <input id="remember_me" type="checkbox"
+                                class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500"
+                                name="remember">
+                            <span class="ms-2 text-sm text-white font-mono font-bold ">{{ __('Remember me') }}</span>
+                        </label>
+                    </div>
+                    @if (Route::has('password.request'))
+                        <a class="underline text-sm text-white font-mono font-bold  hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                            href="{{ route('password.request') }}">
+                            {{ __('Forgot your password?') }}
+                        </a>
+                    @endif
+                </div>
+
+                <div class="mt-6">
+                    <button type="submit"
+                        class=" font-mono font-bold w-full inline-flex items-center justify-center px-4 py-2 bg-purple-700 border border-transparent rounded-md font-semibold capitalize text-white  focus:outline-none disabled:opacity-25 transition">Connexion</button>
+                </div>
+                <div class="mt-6 text-center">
+                    <a href="/" class="underline text-white font-mono font-bold">Retour a l'accueille </a>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
