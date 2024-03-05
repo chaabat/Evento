39985e03e2a5a0c4db4c09d4f7debf91b91409be
chaabat <chaabat.02@gmail.com>
2024-03-05 21:08:14 +0100

    Admin categorie UI

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,74 +1,55 @@
 @extends('layouts.admin')
 @section('home')
-    <!-- component -->
-    <div class="h-screen w-full bg-white relative flex overflow-hidden">
-
-        <!-- Sidebar -->
-        <aside class="h-full w-16 flex flex-col space-y-10 items-center justify-center relative bg-gray-800 text-white">
-            <!-- Profile -->
-            <div
-                class="h-10 w-10 flex items-center justify-center rounded-lg cursor-pointer hover:text-gray-800 hover:bg-white  hover:duration-300 hover:ease-linear focus:bg-white">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
-                    <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd" />
-                </svg>
-            </div>
-
-            <!-- Courses -->
-            <div
-                class="h-10 w-10 flex items-center justify-center rounded-lg cursor-pointer hover:text-gray-800 hover:bg-white  hover:duration-300 hover:ease-linear focus:bg-white">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
-                    stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
-                </svg>
-            </div>
-
-            <!-- Theme -->
-            <div
-                class="h-10 w-10 flex items-center justify-center rounded-lg cursor-pointer hover:text-gray-800 hover:bg-white  hover:duration-300 hover:ease-linear focus:bg-white">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
-                    stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z" />
-                </svg>
-            </div>
-
-            <!-- Configuration -->
-            <div
-                class="h-10 w-10 flex items-center justify-center rounded-lg cursor-pointer hover:text-gray-800 hover:bg-white  hover:duration-300 hover:ease-linear focus:bg-white">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
-                    stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                </svg>
-            </div>
-        </aside>
-
-
-
-        <div class="w-full h-full flex flex-col justify-between">
-
-
-            <!-- Main -->
-            <main class="max-w-full h-full flex relative overflow-y-hidden">
-                <!-- Container -->
-                <div
-                    class="h-full w-full m-4 flex flex-wrap items-start justify-start rounded-tl grid-flow-col auto-cols-max gap-4 overflow-y-scroll">
-                    <!-- Container -->
-                    <div class="w-96 h-60 rounded-lg flex-shrink-0 flex-grow bg-gray-400"></div>
-                    <div class="w-96 h-60 rounded-lg flex-shrink-0 flex-grow bg-gray-400"></div>
-                    <div class="w-96 h-60 rounded-lg flex-shrink-0 flex-grow bg-gray-400"></div>
-                    <div class="w-96 h-60 rounded-lg flex-shrink-0 flex-grow bg-gray-400"></div>
-                    <div class="w-96 h-60 rounded-lg flex-shrink-0 flex-grow bg-gray-400"></div>
-                    <div class="w-96 h-60 rounded-lg flex-shrink-0 flex-grow bg-gray-400"></div>
-                    <div class="w-96 h-60 rounded-lg flex-shrink-0 flex-grow bg-gray-400"></div>
-                    <div class="w-96 h-60 rounded-lg flex-shrink-0 flex-grow bg-gray-400"></div>
-                    <div class="w-96 h-60 rounded-lg flex-shrink-0 flex-grow bg-gray-400"></div>
+    <div class="p-4 sm:ml-64  h-screen overflow-y-scroll"
+        style="background:linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('{{ asset('photos/event.jpg') }}') no-repeat center;background-size:cover">
+        <div class="  rounded-lg mt-14">
+            <div class="rounded-t mb-0 px-4 py-3 border-0 bg-purple-700 ">
+                <div class="flex flex-wrap items-center text-white ">
+                    <div class="relative w-full px-4 max-w-full flex-grow flex-1">
+                        <h3 class="font-bold font-mono text-xl text-white">Categories</h3>
+                    </div>
+                    <div class="relative w-full px-4 max-w-full flex-grow flex-1 text-right">
+                        <span data-modal-target="crud-modal" data-modal-toggle="crud-modal">
+                            <a href="#" id="add-button"
+                                class="bg-white text-purple-700 text-sm font-mono font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1  "
+                                type="button">Ajouter</a>
+                        </span>
+                    </div>
                 </div>
-            </main>
-        </div>
+            </div>
+            <div class="mx-auto max-w-screen-xl px-4 w-full mt-12 mb-12">
+                <div class="grid w-full sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+                    {{-- @foreach ($categorie as $cate) --}}
+                    <div
+                        class="relative flex flex-col shadow-md rounded-xl overflow-hidden hover:shadow-lg hover:-translate-y-1 transition-all duration-300 max-w-sm">
+                        <div class="h-auto overflow-hidden">
+                            <div class="h-44 overflow-hidden relative">
+                                <img src="{{ asset('photos/titre.png') }}" class="h-44 w-full" alt="">
+                            </div>
+                        </div>
+                        <div class="bg-white py-4 px-3">
+                            <h1 class="text-3xl text-black text-center mb-2 font-bold font-mono">nom</h1>
+                            <div class="flex justify-between">
 
+                                <span data-modal-target="crud-modal-update" data-modal-toggle="crud-modal-update">
+                                    <a href="#" class="text-blue-500 hover:text-blue-700 edit-category"><img
+                                            src="{{ asset('photos/editer.png') }}" class="h-10 w-10"></a>
+                                </span>
+
+
+                                <form action="#" method="POST">
+                                    @csrf
+                                    @method('delete')
+                                    <button type="submit" class="text-red-500 hover:text-red-700"><img
+                                            src="{{ asset('photos/supprimer.png') }}" class="h-10 w-10"></button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+
+                    {{-- @endforeach --}}
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -9,13 +9,14 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
     <script src="https://cdn.tailwindcss.com"></script>
-
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
 
 </head>
+@include('incFiles.adminNav')
+
 <body>
 
     @yield('home')
-  
 
 </body>
 
